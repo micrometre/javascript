751ee92c2e082b53e43092bae725ca241f842377
Henok <henokmicrometre@gmail.com>
2021-04-17 11:54:24 +0100

    starting import

---
+++ javascripting/public/js/variations.js
@@ -0,0 +1,42 @@
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    // return the number of characters in: name
+
+}
+
+/** @param {string} name */
+export function getFirstChar(name) {
+    // return the first character of: name
+
+}
+
+/** @param {string} name */
+export function getLastChar(name) {
+    // return the last character of: name
+
+}
+
+/** @param {string} name */
+export function getLower(name) {
+    // return name all in lower case (example: "ABC" becomes "abc")
+
+}
+
+/** @param {string} name */
+export function getUpper(name) {
+    // return name all in upper case (example: "abc" becomes "ABC")
+
+}
+
+/** @param {string} name */
+export function getCapitalized(name) {
+    // return a capitalized version of name (example: "alEX" becomes "Alex")
+
+}
+
+/** @param {string} name */
+export function getClean(name) {
+    // return name without trailing and leading space (example: " alex " becomes: "alex")
+
+}
+
