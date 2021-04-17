e4b7f522b7bf93ebc83171eb1baf2f092bf0ec33
Henok <henokmicrometre@gmail.com>
2021-04-17 16:53:31 +0100

    working on functions

---
+++ javascripting/public/js/string-1.js
@@ -0,0 +1,7 @@
+/** @param {string} name */
+function getCapitalized(name) {
+    // return a capitalized version of name (example: "alEX" becomes "Alex")
+        return name[0].toUpperCase()
+
+}
+getCapitalized("alex")
