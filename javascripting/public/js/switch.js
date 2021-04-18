6098db79a57b122d706b29bc0db998cceba14fed
Henok <henokmicrometre@gmail.com>
2021-04-18 15:50:14 +0100

    return-booleans.js

---
+++ javascripting/public/js/switch.js
@@ -0,0 +1,15 @@
+    let expr = 'Papayas';
+    switch (name) {
+        case "Caleb":
+            console.log("welcome");
+            break;
+        case "Claire":
+            console.log("Get out here");
+            break;
+        default: 
+            console.log("I guess your're welcome'");
+            break;
+
+    }
+
+
