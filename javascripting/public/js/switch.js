83a7b442b7ae33caf6140560f91947cd588626cc
henok <henokmicrometre@gmail.com>
2021-04-19 11:39:33 +0100

    working switch

--- javascripting/public/js/switch.js
+++ javascripting/public/js/switch.js
@@ -1,3 +1,34 @@
+function chainToSwitch(val) {
+  var answer = "";
+switch(val) {
+	case "bob":
+    		answer = "Marley";
+		break;
+	case 42:
+		answer = "The Answer";
+		break;
+	case 1:
+		answer	= "There is no #1";	
+		break;
+	case 99:
+		answer = "Missed me by this much!";
+		break;
+	case 7:
+    		answer = "Ate Nine";
+		break;
+	case "Jhon":
+		answer = "";
+		break;
+	case 156:
+		answer = "";
+		break;
+	
+}
+  return answer;
+}
+chainToSwitch(7);
+/*
+
 function switchOfStuff(val) {
     var answer = "";
     switch (val) {
@@ -42,3 +73,4 @@ function sequentialSizes(val) {
     return answer;
 }
 sequentialSizes(1);
+*/
