620ec7ca4dcd56427b4883db283ae5481d372f1b
henok <henokmicrometre@gmail.com>
2021-04-19 07:37:15 +0100

    working switch

--- javascripting/public/js/switch.js
+++ javascripting/public/js/switch.js
@@ -1,6 +1,5 @@
 function switchOfStuff(val) {
     var answer = "";
-    // Only change code below this line
     switch (val) {
     case "a":
         answer = "apple";
@@ -17,9 +16,29 @@ function switchOfStuff(val) {
         answer = "stuff";
 
     }
-
-    // Only change code above this line
     return answer;
 }
 
 switchOfStuff(1);
+
+function sequentialSizes(val) {
+    var answer = "";
+    switch (val) {
+    case 1:
+    case 2:
+    case 3:
+        answer = "Low";
+        break;
+    case 4:
+    case 5:
+    case 6:
+        answer = "Mid";
+        break
+    case 7:
+    case 8:
+    case 9:
+        answer = "High"
+    }
+    return answer;
+}
+sequentialSizes(1);
