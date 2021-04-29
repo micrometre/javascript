15f6ebb4f3abeaa03242c4bb63e879a31691ce2b
henok <henokmicrometre@gmail.com>
2021-04-29 23:51:20 +0100

    working with

---
+++ javascripting/public/js/boolean.js
@@ -0,0 +1,34 @@
+
+let base = prompt('Enter base');
+let hight = prompt('Enter hight');
+let area = 0.5 * base * hight
+
+alert(`The area of the triangle is ${area}`)
+
+/*
+    var b,h,area;
+    b= parseFloat(prompt("enter base"));
+    h =parseFloat(prompt("enter height"));
+    area = (0.5*b)*h;
+    document.write('base is:' +b+ '<br>');
+    document.write('height is:' +h+ '<br>');
+    document.write('Area of this triangle is:' +area); 
+
+
+/*
+let firstName = 'Peter';
+let latName = 'Grifith'
+let Country = 'USA';
+let City = 'Quahog';
+let Age = 50;
+let Status = 'isMarried';
+
+var familyGuy = {
+firstName:'Peter',
+lastName: 'Grifith',
+Country : 'USA',
+City : 'Quahog',
+Age : 50,
+Status : 'isMarried'
+};
+*/
