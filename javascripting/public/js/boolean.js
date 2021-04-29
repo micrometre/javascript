9a5b86f80f210130dd89d650720615d86f99bc49
henok <henokmicrometre@gmail.com>
2021-04-30 00:50:52 +0100

    working with document

--- javascripting/public/js/boolean.js
+++ javascripting/public/js/boolean.js
@@ -1,21 +1,35 @@
+//calculate the perimeter of triangle
+var a,b,c,perimeter;
+a=parseFloat(prompt("Enter side a"));
+b=parseFloat(prompt("Enter side b"));
+c=parseFloat(prompt("Enter side c"));
+perimeter = a + b + c ;
+document.write('side a is:' +a+ '<br>');
+document.write('side b is:' +b+ '<br>');
+document.write('side c  is:' +c+ '<br>');
+document.write('The perimeter of the triangle is:' +perimeter); 
+
+
+
+
+
+/*
+var b,h,area;
+b= parseFloat(prompt("enter base"));
+h =parseFloat(prompt("enter height"));
+area = (0.5*b)*h;
+document.write('base is:' +b+ '<br>');
+document.write('height is:' +h+ '<br>');
+document.write('Area of this triangle is:' +area); 
+
+
+
 
 let base = prompt('Enter base');
 let hight = prompt('Enter hight');
-let area = 0.5 * base * hight
+let area =(0.5*base)*hight; 
 
 alert(`The area of the triangle is ${area}`)
-
-/*
-    var b,h,area;
-    b= parseFloat(prompt("enter base"));
-    h =parseFloat(prompt("enter height"));
-    area = (0.5*b)*h;
-    document.write('base is:' +b+ '<br>');
-    document.write('height is:' +h+ '<br>');
-    document.write('Area of this triangle is:' +area); 
-
-
-/*
 let firstName = 'Peter';
 let latName = 'Grifith'
 let Country = 'USA';
