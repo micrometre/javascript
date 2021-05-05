b11ee8e6482a368b28eef190465f1bfc2a521688
henok <henokmicrometre@gmail.com>
2021-05-05 22:14:42 +0100

    first module

--- learn-func/server.js
+++ learn-func/server.js
@@ -11,6 +11,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+app.get('/api', function(req, res) {
+    res.send(upperCaser('hello'));
+});
 
 console.log(upperCaser('hello'));
 app.listen(8080);
