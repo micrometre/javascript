8164773c59b1dee8d0703bd1e6afb3cb26cad66c
henok <henokmicrometre@gmail.com>
2021-06-01 21:52:09 +0100

    starting lesson

--- basics/public/js/functions.js
+++ basics/public/js/functions.js
@@ -1,3 +1,16 @@
+// this function takes array as a parameter and sum up the numbers in the array
+function sumArrayValues(arr) {
+  
+var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum = sum + numbers[i];
+  }
+  return sum;
+}
+const numbers = [1, 2, 3, 4, 5];
+console.log(sumArrayValues(numbers));
+
+/*
 function inYen(dollars, ) {
     let amount = dollars * 113;
     return amount;
@@ -8,4 +21,5 @@ function inYen(dollars, ) {
 function eat(food) {
     return food + ' tasted really good.'
 }
-//console.log(eat("bananas"))
+console.log(eat("bananas"))
+*/
